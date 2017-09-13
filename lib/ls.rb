3e36be47536b1d86e7007fbbdb08aaf405f0a26c
Akshatha Ballal <akshatha.ballal@gmail.com>
2017-09-13 14:46:49 +0100

    Added checks for a flag

--- lib/ls.rb
+++ lib/ls.rb
@@ -1,4 +1,23 @@
 class Ls
+  def initialize
+    @tips = {
+      a: '-a would give you hidden files as well'
+    }
+  end
+
   def suggest_tips(command)
+    options = []
+    command.split(/-/)[1..-1].each do |option|
+      option.each_char do |char|
+        options << char
+      end
+    end
+    [] << check_for_a(options)
+  end
+
+  private
+
+  def check_for_a(options)
+    return @tips[:a] unless options.include?("a")
   end
 end
