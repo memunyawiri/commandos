1ed7368026baf77c8ca81c0839f701324575e086
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2017-09-13 16:38:08 +0100

    Added tip for -lt flag

--- lib/ls.rb
+++ lib/ls.rb
@@ -3,14 +3,16 @@ class Ls
     @tips = {
       a: '-a would give you hidden files as well',
       l: '-l would give you more information about your files',
-      la: 'Did you know, you can also run “ls -la” to get more information about your ALL your files!'
+      la: 'Did you know, you can also run “ls -la” to get more information about your ALL your files!',
+      lt: '-lt gives you a list of files sorted by time modified :-)'
     }
   end
 
   def suggest_tips(command)
-    options = []
+    options = [command.split(/-/).last]
     command.split(/-/)[1..-1].each do |option|
       option.each_char do |char|
+        puts char
         options << char
       end
     end
@@ -20,7 +22,11 @@ class Ls
   private
 
   def combine_all_useful_tips(options)
-    [] << check_for_a(options) << check_for_l(options) << check_for_la(options)
+    [] << check_for_a(options) << check_for_l(options) << check_for_la(options) << check_for_lt(options)
+  end
+
+  def check_for_lt(options)
+    return @tips[:lt] unless options.include?('lt')
   end
 
   def check_for_la(options)
--- spec/ls_spec.rb
+++ spec/ls_spec.rb
@@ -5,11 +5,12 @@ describe Ls do
   let!(:tip_a) { "-a would give you hidden files as well" }
   let!(:tip_l) { "-l would give you more information about your files" }
   let!(:tip_la) { "Did you know, you can also run “ls -la” to get more information about your ALL your files!" }
+  let!(:tip_lt) { "-lt gives you a list of files sorted by time modified :-)" }
 
   describe '#suggest_tips' do
     describe "Edge cases" do
       it "passes all of the tips if no of the flags are used" do
-        expect(ls.suggest_tips("ls")).to eq([tip_a, tip_l, tip_la])
+        expect(ls.suggest_tips("ls")).to eq([tip_a, tip_l, tip_la, tip_lt])
       end
     end
 
@@ -47,7 +48,17 @@ describe Ls do
       end
 
       it 'does not suggest using -la option when already used in a combined format' do
-        expect(ls.suggest_tips("ls -la").include?(tip_l)).to be false
+        expect(ls.suggest_tips("ls -la").include?(tip_la)).to be false
+      end
+    end
+
+    describe 'Tip for "-lt" flag' do
+      it 'can suggest using -lt option when it is not used' do
+        expect(ls.suggest_tips("ls").include?(tip_lt)).to be true
+      end
+
+      it 'does not suggest using -lt option when already used' do
+        expect(ls.suggest_tips("ls -lt").include?(tip_lt)).to be false
       end
     end
 
