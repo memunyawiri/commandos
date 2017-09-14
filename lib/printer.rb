38e28cda922c697e8336397f1f6c5e41462ed8b1
EOSullivanBerlin <esully666@gmail.com>
2017-09-14 18:57:08 +0200

    Printer Class created with private methods tested thorugh main public method

--- lib/printer.rb
+++ lib/printer.rb
@@ -1,7 +1,35 @@
 class Printer
 
-  def standard_output
-    print "You can switch between 2 directories using cd -"
+
+  def output(tips, type_of_output)
+    output_type_decider(tip_decider(tips), type_of_output)
+  end
+
+private
+
+  def output_type_decider(tip, type_of_output)
+    print_tips(tip) if type_of_output == :print
+    write_tips_to_file(tip) if type_of_output == :file
+    say_tips(tip) if type_of_output == :says
+  end
+
+  def tip_decider(tips)
+    tips.first
+  end
+
+  def print_tips(tip)
+    print tip
+  end
+
+  def write_tips_to_file(tip)
+    file = "output/output.txt"
+    output_file = File.open(file, "w")
+    output_file.puts tip
+    output_file.close
+  end
+
+  def say_tips(tip)
+    system "say", tip
   end
 
 end
--- spec/printer_spec.rb
+++ spec/printer_spec.rb
@@ -1,11 +1,27 @@
 require 'printer'
+
 describe Printer do
   subject(:printer) { described_class.new }
-  let(:expected_string) {"You can switch between 2 directories using cd -"}
+  let!(:tips_array) { [tip_a, tip_l, tip_t] }
+  let!(:tip_a) { '-a would give you hidden files as well' }
+  let!(:tip_l) { '-l would give you more information about your files' }
+  let!(:tip_t) { '-t gives you a list of files sorted by time modified :-)' }
 
-  describe "standard_output" do
+
+
+  describe "output" do
     it 'puts the tip in pretty way' do
-      expect { printer.standard_output }.to output(expected_string).to_stdout
+      expect { printer.output(tips_array, :print) }.to output(tip_a).to_stdout
     end
+
+    it 'prints a tip to a file' do
+      printer.output(tips_array, :file)
+       expect(exist?("output.txt")).to be true
+    end
+
+    #
+
+
   end
+
 end
