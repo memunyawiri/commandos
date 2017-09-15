9ad3a4d3f6954568ab9fbb090a88ff9b3a2556b6
Akshatha Ballal <akshatha.ballal@gmail.com>
2017-09-15 15:40:14 +0100

    Tidied up Module Flags

--- lib/flags.rb
+++ lib/flags.rb
@@ -1,7 +1,7 @@
 module Flags
   def extract_flags(arguments)
     flags = arguments.split(' ').select { |part| part[0] == '-'}
-    abc = flags.each_with_object([]) do |flag, individual_flags|
+    flags.each_with_object([]) do |flag, individual_flags|
       flag.each_char do |char|
         individual_flags << char unless char == '-'
       end
