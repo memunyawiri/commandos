e3deb83b500f7b9bc7576f1db5ff6c72acc9f4df
EOSullivanBerlin <esully666@gmail.com>
2017-09-17 14:55:54 +0200

    Logic for branch and log created

--- lib/git.rb
+++ lib/git.rb
@@ -1,14 +1,37 @@
 class Git
   include Tips
+  include Flags
   def initialize(filename = 'tips/git.txt')
     @tips = load_tips(filename)
   end
 
-  def suggest_tips(_arguments)
-    [@tips[:branch], @tips[:log]]
+  def suggest_tips(arguments)
+    arguments
+    combined_tips(find_the_second_part_of_the_argument(arguments))
+    #[@tips[:branch], @tips[:log]]
   end
 
   def tips
     @tips.dup
   end
+
+  private
+
+  def combined_tips(options)
+    [check_for_branch(options), check_for_log(options)]
+  end
+
+  def find_the_second_part_of_the_argument(arguments)
+    p arguments.split(" ")
+
+  end
+
+  def check_for_branch(options)
+    return @tips[:branch] if options.include?('branch') && !options.include?('-r')
+  end
+
+  def check_for_log(options)
+    return @tips[:log] if !options.include?('log')
+  end
+
 end
--- spec/git_spec.rb
+++ spec/git_spec.rb
@@ -22,11 +22,19 @@ describe Git do
           it 'suggests using -r when branch is used' do
             expect(git.suggest_tips('branch').include?(tips[:branch])).to be true
           end
+
+          it 'does not suggests using -r when branch -r is used' do
+            expect(git.suggest_tips('branch -r').include?(tips[:branch])).to be false
+          end
         end
 
         describe 'Tip for log' do
           it 'suggests using log when git is used' do
             expect(git.suggest_tips('').include?(tips[:log])).to be true
+          end
+
+          it 'does not suggest using log tip when log is used' do
+            expect(git.suggest_tips('log').include?(tips[:log])).to be false
           end
         end
       end
