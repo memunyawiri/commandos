c073704a73d3a6a77dadce5e02846f9792e7c38c
EOSullivanBerlin <esully666@gmail.com>
2017-09-18 12:05:06 +0200

    Pass the review

--- lib/git.rb
+++ lib/git.rb
@@ -6,9 +6,7 @@ class Git
   end
 
   def suggest_tips(arguments)
-    arguments
-    combined_tips(find_the_second_part_of_the_argument(arguments))
-    #[@tips[:branch], @tips[:log]]
+    combined_tips(arguments.split(' '))
   end
 
   def tips
@@ -21,17 +19,12 @@ class Git
     [check_for_branch(options), check_for_log(options)]
   end
 
-  def find_the_second_part_of_the_argument(arguments)
-    p arguments.split(" ")
-
-  end
-
   def check_for_branch(options)
     return @tips[:branch] if options.include?('branch') && !options.include?('-r')
   end
 
   def check_for_log(options)
-    return @tips[:log] if !options.include?('log')
+    return @tips[:log] unless options.include?('log')
   end
 
 end
