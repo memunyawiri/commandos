585cc71e231561682eee64a16bc809d3b3a9c563
Mutsa <mutsa.evelyn@hotmail.com>
2017-09-19 15:41:50 +0100

    Fixed rubocop offenses.

--- lib/cat.rb
+++ lib/cat.rb
@@ -8,7 +8,7 @@ class Cat
   end
 
   def suggest_tips(arguments)
-    non_flags = arguments.split(' ').reject { |part| part[0] == '-'}
+    non_flags = arguments.split(' ').reject { |part| part[0] == '-' }
     flags = extract_flags(arguments)
     tips.map { |key, value| value unless (flags + non_flags).include?(key.to_s) }
   end
--- lib/mkdir.rb
+++ lib/mkdir.rb
@@ -9,7 +9,7 @@ class Mkdir
 
   def suggest_tips(arguments)
     # tips.values
-    non_flags = arguments.split(' ').reject { |part| part[0] == '-'}
+    non_flags = arguments.split(' ').reject { |part| part[0] == '-' }
     flags = extract_flags(arguments)
     tips.map { |key, value| value unless (flags + non_flags).include?(key.to_s) }
   end
