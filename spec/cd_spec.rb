26461a277c2f6af070f98ebb75e9f107aee11c0d
Mutsa <mutsa.evelyn@hotmail.com>
2017-09-19 15:42:34 +0100

    Changed CD to the generic absence of the key logic.

--- spec/cd_spec.rb
+++ spec/cd_spec.rb
@@ -20,35 +20,31 @@ describe Cd do
   describe '#suggest_tips' do
     describe 'Tip for hyphen' do
       it 'suggests using the hyphen when it is not used' do
-        expect(cd.suggest_tips('ruby-kickstart').include?(tips[:hyphen])).to be true
+        expect(cd.suggest_tips('ruby-kickstart').include?(tips[:-])).to be true
       end
 
       it 'does not suggest using the hyphen option when already used' do
-        expect(cd.suggest_tips('- ').include?(tips[:hyphen])).to be false
+        expect(cd.suggest_tips('- ').include?(tips[:-])).to be false
       end
     end
 
     describe 'Tip for tilde' do
       it 'suggests using the tilde when it is not used' do
-        expect(cd.suggest_tips('ruby-kickstart').include?(tips[:tilde_or_empty])).to be true
+        expect(cd.suggest_tips('ruby-kickstart').include?(tips[:~])).to be true
       end
 
       it 'does not suggest using the tilde option when already used alone' do
-        expect(cd.suggest_tips('~').include?(tips[:tilde_or_empty])).to be false
-      end
-
-      it 'does not suggest using the tilde option when already used in a path' do
-        expect(cd.suggest_tips('~/projects').include?(tips[:tilde_or_empty])).to be false
+        expect(cd.suggest_tips('~').include?(tips[:~])).to be false
       end
     end
 
     describe 'Tip for forward slash' do
       it 'suggests using the forward slash when it is not used' do
-        expect(cd.suggest_tips('ruby-kickstart').include?(tips[:forward_slash])).to be true
+        expect(cd.suggest_tips('ruby-kickstart').include?(tips[:/])).to be true
       end
 
       it 'does not suggest using the forward slash option when already used' do
-        expect(cd.suggest_tips('/').include?(tips[:forward_slash])).to be false
+        expect(cd.suggest_tips('/').include?(tips[:/])).to be false
       end
     end
 
