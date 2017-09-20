56954a6c24d531f0d9ba1abfa1750359d04d9986
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2017-09-20 02:59:37 +0100

    tags update

--- install/install.sh
+++ install/install.sh
@@ -1,5 +1,5 @@
 echo Downloading and installing commandos app...
-VER=1.1
+VER=1.2
 curl -L https://github.com/BDCraven/commandos/archive/$VER.tar.gz | tar vzx
 echo ...DONE
 echo "export PATH=\$PATH:$PWD/commandos-$VER/bin" >> ~/.bash_profile
