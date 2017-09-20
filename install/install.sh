51dfa8dc0f799c80e13f34b96536cb94ea7c5394
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2017-09-20 01:41:36 +0100

    Third step for install

--- install/install.sh
+++ install/install.sh
@@ -1,4 +1,4 @@
 echo Downloading and installing commandos app...
 curl -L https://github.com/BDCraven/commandos/archive/1.0.tar.gz | tar vzx
 echo ...DONE
-echo Current working directory is $PWD
+echo "export PATH=\$PATH:$PWD/bin" >> ~/.bash_profile
